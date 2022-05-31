3286d34ae5d85d7dcf1fb31c2b42391e396025bc
mjLamarree <67963551+mjLamarree@users.noreply.github.com>
2022-05-31 15:23:07 -0400

    Function consalidation
    moved both functions into what will be the main callback function for the search result event.

--- weatherApp.js
+++ weatherApp.js
@@ -3,10 +3,10 @@ const weatherCardCurrentTemp = document.getElementById('weatherCardCurrentTemp')
 const weatherCardCloudCoverage = document.getElementById('weatherCardCloudCoverage')
 const weatherCardMaxMinTemp = document.getElementById('weatherCardMaxMinTemp')
 
-
-//needs to be updated with placeholder for fetch link, will need to intergrate API for city name to lat long
-async function getWeather(){
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=7c718d10bb6da76a31c5194ce833087c')
+//add functionality for converting city name to lat,lon and temp to farenheight
+function getWeatherData(){
+    async function getWeather(lat,lon){
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7c718d10bb6da76a31c5194ce833087c`)
 
     if(!response.ok){
         let message = `An error has occured: ${response.status}`
@@ -17,11 +17,12 @@ async function getWeather(){
     return weatherData;
 }
 
-
-getWeather().then(function(data){
+getWeather(35,139).then(function(data){
    console.log(data)
    weatherCardCity.textContent = data.name
    weatherCardCurrentTemp.textContent = data.main.temp
    weatherCardMaxMinTemp.textContent = `High of ${data.main.temp_max} , Low of ${data.main.temp_min}`
    weatherCardCloudCoverage.textContent = data.weather[0].description
 })
+}
+
