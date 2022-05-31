c17a25179e9b993f3788a57ea266d0dd54c2126e
mjLamarree <67963551+mjLamarree@users.noreply.github.com>
2022-05-31 14:28:55 -0400

    New API call + more
    Updated the API call method async, easier promise handling, also added a placeholder weather card with some basic spots for info and declared them in the js

--- weatherApp.js
+++ weatherApp.js
@@ -1,8 +1,18 @@
+const weatherCardCity = document.getElementById('weatherCardCity')
+const weatherCardCurrentTemp = document.getElementById('weatherCardCurrentTemp')
+const weatherCardCloudCoverage = document.getElementById('weatherCardCloudCoverage')
+const weatherCardMaxMinTemp = document.getElementById('weatherCardMaxMinTemp')
 
 
-window.fetch('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=7c718d10bb6da76a31c5194ce833087c')
-.then(response=>{
-    return response.json();
-}).then(json=>{
-    console.log(json)
-})
+//needs to be updated with placeholder for fetch link, will need to intergrate API for city name to lat long
+async function getWeather(){
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=7c718d10bb6da76a31c5194ce833087c')
+
+    if(!response.ok){
+        let message = `An error has occured: ${response.status}`
+        throw new Error(message);
+    }
+
+    const weatherData = await response.json();
+    return weatherData;
+}
